e85d15329921340754fdfd8cb653e725c0c4e797
Kiley Bobbitt <jkileybob@aol.com>
2018-11-17 16:30:15 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,13 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? {|word| word.start_with?("r")}
+end
+
+def contain_a(array)
+  array.select {|letter_a| letter_a.include?("a")}
+end
+
+def first_wa (array)
+  array.select do |wa|
+    wa.to_s.start_with?("wa")
+  end
+end
